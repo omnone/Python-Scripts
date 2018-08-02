400ee180887bcff13ea0004b724669dc490c2676
Kostas Bourantas <bourantas@ceid.upatras.gr>
2018-08-02 19:34:03 +0300

    Update file_backup.py

--- file_backup.py
+++ file_backup.py
@@ -1,3 +1,7 @@
+#This scripts helps you to backup the contents of a directory
+#files and directories fast . Simply write the source path and 
+#the path of the folder you wish to save your files to.
+
 import os, sys
 import shutil
 import errno
