dbae12765f97551016ba628d0c121929e4ed7570
Kostas Bourantas <bourantas@ceid.upatras.gr>
2019-02-17 19:50:18 +0200

    Update pdf_metadata_check.py

--- pdf_metadata_check.py
+++ pdf_metadata_check.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-import PyPDF2, os, csv
+import PyPDF2
+import os
+import csv
 
-#get all pdf files from directory
-pdf_files=[filename for filename in os.listdir('.') if filename.endswith('.pdf')]
+# get all pdf files from directory
+pdf_files = [filename for filename in os.listdir(
+    '.') if filename.endswith('.pdf')]
 
 
 with open('metadata_check.csv', mode='w', newline='') as check_file:
-        
-     csv_writer = csv.writer(check_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-     csv_writer.writerow(['File:','Has_Meta:','Metadata:'])
-        
-     results = []
 
-     for filename in pdf_files:
-             pdfWriter = PyPDF2.PdfFileWriter()
-             pdfFileObj = open(filename,'rb')
-             pdfReader = PyPDF2.PdfFileReader(pdfFileObj,strict=False)
-        
-             metadata = pdfReader.getDocumentInfo()
-             #print(metadata)
-             meta_found= [key.strip('/').lower()+': '+value for key,value in metadata.items()
-                          if value and key!='/Producer' and key!='/CreationDate'
-                          and key!='/ModDate']
+    csv_writer = csv.writer(check_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(['File:', 'Has_Meta:', 'Metadata:'])
 
-             if meta_found:
-                #print(f'[-]:{filename} has metadata:')
-                #for item in meta_found:
-                   #print(item)
-                #print('--------------------------')
-                results.append([filename,'True',meta_found])
-             else:
-                results.append([filename,'False','-'])
+    results = []
 
-                
-     csv_writer.writerows(sorted(results, key=lambda x: x[1],reverse=True))                
-             
-                
-                     
+    for filename in pdf_files:
+        pdfWriter = PyPDF2.PdfFileWriter()
+        pdfFileObj = open(filename, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False)
+
+        metadata = pdfReader.getDocumentInfo()
+        # print(metadata)
+        meta_found = [key.strip('/').lower()+': '+value for key, value in metadata.items()
+                      if value and key != '/Producer' and key != '/CreationDate'
+                      and key != '/ModDate']
+
+        if meta_found:
+            #print(f'[-]:{filename} has metadata:')
+            # for item in meta_found:
+              # print(item)
+            # print('--------------------------')
+            results.append([filename, 'True', meta_found])
+        else:
+            results.append([filename, 'False', '-'])
+
+    csv_writer.writerows(sorted(results, key=lambda x: x[1], reverse=True))
